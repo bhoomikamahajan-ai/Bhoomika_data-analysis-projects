eb687913f11ef26485d8c5b1b889339ac96564a5
Bhoomika <bhoomika.mahajan@gmail.com>
2025-10-30 22:01:38 -0500

    Exercise python data and variables

---
+++ class_demo.py
@@ -0,0 +1,38 @@
+'''print(type(5))
+print(type(5.0))
+print(type("5"))
+
+print(5 * 2)
+
+print("5" * 2)
+
+budget = "1000"
+print(type(budget))
+print(budget * 2)
+budget = int(budget)
+print( budget * 2)
+print("Data" + "Science")
+
+print(5  + 5)
+salary = 80000
+print ("salary:" + str(salary))
+
+
+year = 2025
+month = 10
+# print("Date:" + year + "-" + month)
+print("Date:" + str(year) + "-" + str(month))
+
+print("Date:", year,"-",month)
+
+print(f"Date: {year} - {month}") # formatted string
+
+
+print(f"Banan stand Profit: ${100-20}")
+
+a = 2
+b = 3
+result = a ** b + (10-4)/2
+print("Result:",round(result))
+'''
+print(10 % 3)
--- data-and-variables/exercises/exercises.py
+++ data-and-variables/exercises/exercises.py
@@ -15,4 +15,18 @@ print(type(MILES_PER_KM))
 
 # Code your solution to exercises 3 and 4 here:
 
+distance_to_mars_miles = distance_to_mars_km * MILES_PER_KM
+time_to_reach_mars = distance_to_mars_miles / shuttle_speed_mph
+days_to_reach_mars = time_to_reach_mars / 24
+
+print(days_to_reach_mars);
+
+print(f"{shuttle_name} will take {days_to_reach_mars:.2f} days to reach Mars.")
 # Code your solution to exercise 5 here
+
+distance_to_moon_miles = distance_to_moon_km * MILES_PER_KM
+time_to_reach_moon = distance_to_moon_miles / shuttle_speed_mph
+days_to_reach_moon = time_to_reach_moon / 24
+print (days_to_reach_moon);
+
+print(f"{shuttle_name} will take {days_to_reach_moon:.2f} days to reach Moon.")
