1776f4dc2a5ff2364dd9d03e130df6deef244d74
Bhoomika <bhoomika.mahajan@gmail.com>
2025-11-11 22:36:59 -0600

    functions assignment

---
+++ functions/exercises/tempCodeRunnerFile.py
@@ -0,0 +1,74 @@
+# Part 1 A -- Make a Line
+
+# def make_line(size):
+#     line = ""
+#     for i in range(size):
+#         line += '#'
+#     return line
+
+# # Part 1 B -- Make a Square
+# # create a function using your make_line function to code a square
+
+# def make_square(size):
+#     square = ""
+#     for i in range(size):
+#         square += make_line(size) 
+#         if i < size -1 :         # no newline after last line
+#             square += "\n"
+#     return square 
+
+# # Part 1 C -- Make a Rectangle
+
+# def make_rectangle(width, height):
+#     rectangle = ""
+#     i = 1
+#     while i <= height:
+#         rectangle += make_line(width) 
+#         if i < height:          # no newline after last line
+#             rectangle += "\n"
+#         i +=1
+#     return rectangle
+
+# print(make_line(2))
+# print(make_square(4))   
+# print(make_rectangle(6, 3))
+
+# # Part 2 A -- Make a Stairs
+
+# # def make_downward_stairs(height):
+    
+# def make_downward_stairs(height):
+#     stairs = ""
+#     for i in range(1, height + 1):
+#         stairs += make_line(i)
+#         if i < height:          # no newline after the last step
+#             stairs += "\n"
+#     return stairs
+
+
+# print(make_downward_stairs(5))
+
+
+# # Part 2 B -- Make Space-Line ---####---
+
+# def make_space_line(numSpaces, numChars):
+#     spaces = " " * numSpaces
+#     chars = make_line(numChars)
+#     return spaces + chars + spaces
+
+# print(make_space_line(4,4)) 
+
+# # Part 2 C -- Make Isosceles Triangle
+
+# def make_isosceles_triangle(height):
+#     triangle = ""
+#     for i in range(height):
+#         spaces = " " * (height - i - 1)
+#         hashes = make_line(2 * i + 1)
+#         triangle += spaces + hashes + spaces 
+#         if i < height-1:
+#             triangle += '\n'            
+
+#     return triangle
+# print(make_isosceles_triangle(5))
+
